447744e805e3260d403b160b0448bfcec2d0c343
ArnoldQuiros <arnoldlquiros@gmail.com>
2023-05-25 04:35:37 +0800

    All new changes to invcontroller will be here

--- ownerhome.php
+++ ownerhome.php
@@ -8,12 +8,26 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name']))
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Home Page</title>
+        <title>Owner Page</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <h1> HELLO, <?php echo $_SESSION['user_name']; ?> </h1>
-        <a href="logout.php">Logout</a>
+    <div class="wrapper">    
+        <div class="sidebar">
+        <p class="navtitle"> Hello, <?php echo $_SESSION['user_name']; ?> </p>
+            <ul class="navbarul">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                    <a href="logout.php">Logout</a>
+                    </li>
+            </ul>
+        </div>
+    </div>    
     </body>
     </html>
 
