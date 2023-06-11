775ca552f588cd920e4eaf14507eb3f87a5d4624
RafayeII <rafael_sanchez@dlsu.edu.ph>
2023-06-11 11:34:41 +0800

    Adjust for Multiple Dish

--- Ordering System/process_order.php
+++ Ordering System/process_order.php
@@ -13,27 +13,33 @@
       $dishName = $_POST['vDish'];
       $qty = $_POST['vQuantity'];
 
-      $priceQuery = mysqli_query($DBConnect, "SELECT price FROM dish WHERE dishName = '$dishName';");
+      for ($i = 0; $i < count($qty); $i++) {
+        $priceQuery = mysqli_query($DBConnect, "SELECT price FROM dish WHERE dishName = '$dishName[$i]';");
 
-      $dish = mysqli_fetch_array($priceQuery);
-      $totalPrice = $dish["price"] * $qty;
+        $dish = mysqli_fetch_array($priceQuery);
+        $totalPrice = $dish["price"] * $qty[$i];
+      }
 
-      // Insert entries to Order and Order_Item tables
       $orderQuery = mysqli_query($DBConnect, "INSERT INTO orders (totalPrice, createdAt) VALUES ($totalPrice, NOW());");
-      $orderItemQuery = mysqli_query($DBConnect, "INSERT INTO order_item (orderID, dishID, quantity) VALUES ((SELECT orderID FROM orders ORDER BY createdAt DESC LIMIT 1), (SELECT dishID FROM dish WHERE dishName = '$dishName'), $qty);");
+    
+      for ($i = 0; $i < count($qty); $i++) {
 
-      // Gets the array list of ingredients using ingredientID
-      $ingredientsQuery = mysqli_query($DBConnect, "SELECT i.ingredientID FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID      WHERE d.dishName = '$dishName';");
+        // Insert entries to Order and Order_Item tables
+        $orderItemQuery = mysqli_query($DBConnect, "INSERT INTO order_item (orderID, dishID, quantity) VALUES ((SELECT orderID FROM orders ORDER BY createdAt DESC LIMIT 1), (SELECT dishID FROM dish WHERE dishName = '$dishName[$i]'), $qty[$i]);");
 
-      // Loop to subtract all the ingredients found on the dish to Inventory base from Recipe table
-      foreach ($ingredientsQuery as $ingredient) {
-        $ingredientQtyQuery = mysqli_query($DBConnect, "SELECT i.quantity FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID WHERE i.ingredientID = " . $ingredient['ingredientID']);
-        $ingredientQty = mysqli_fetch_array($ingredientQtyQuery)['quantity'];
+        // Gets the array list of ingredients using ingredientID
+        $ingredientsQuery = mysqli_query($DBConnect, "SELECT i.ingredientID FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID      WHERE d.dishName = '$dishName[$i]';");
 
-        $subtractQtyQuery = mysqli_query($DBConnect, "SELECT r.quantity FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID WHERE i.ingredientID = " . $ingredient['ingredientID']);
-        $subtractQty = mysqli_fetch_array($subtractQtyQuery)['quantity'];
+        // Loop to subtract all the ingredients found on the dish to Inventory base from Recipe table
+        foreach ($ingredientsQuery as $ingredient) {
+          $ingredientQtyQuery = mysqli_query($DBConnect, "SELECT i.quantity FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID WHERE i.ingredientID = " . $ingredient['ingredientID']);
+          $ingredientQty = mysqli_fetch_array($ingredientQtyQuery)['quantity'];
 
-        $updateQtyQuery =  mysqli_query($DBConnect, "UPDATE ingredient SET quantity = ( $ingredientQty - $subtractQty ) WHERE ingredientID = " . $ingredient['ingredientID']);
+          $subtractQtyQuery = mysqli_query($DBConnect, "SELECT r.quantity FROM recipe r JOIN dish d ON d.dishID = r.dishID JOIN ingredient i ON r.ingredientID = i.ingredientID WHERE i.ingredientID = " . $ingredient['ingredientID']);
+          $subtractQty = mysqli_fetch_array($subtractQtyQuery)['quantity'];
+
+          $updateQtyQuery =  mysqli_query($DBConnect, "UPDATE ingredient SET quantity = ( $ingredientQty - $subtractQty ) WHERE ingredientID = " . $ingredient['ingredientID']);
+        }
       }
     ?>
   </body>
