7efed6a076a71543565a265fe5312729cb63cbd8
VibarEnrique <104816497+VibarEnrique@users.noreply.github.com>
2023-06-15 02:24:22 +0800

    Update chefhome.php

--- chefhome.php
+++ chefhome.php
@@ -5,15 +5,46 @@ session_start();
 {
     ?>
 
-    <!DOCTYPE html>
-    <html>
+   <html>
     <head>
-        <title>Home Page</title>
+        <title>Chef Page</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1> HELLO, <?php echo $_SESSION['user_name']; ?> </h1>
-        <a href="logout.php">Logout</a>
+    <?php @include 'navbar.php' ?>
+    <main>
+                <div class="notifications">
+                    <h2>Notifications</h2>
+                    <ul>
+                        <li>New Ingredient added: Tomato</li>
+                        <li>New Ingredient added: Noodles</li>
+                    </ul>
+                </div>
+                <div class="msales">
+                <h2> Pending Recipes </h2>
+                <h3>Recipe 6</h3>
+                </div>
+            <div class="restocklog">
+            <h2> Processed Recipes </h2>
+                <div class="table">
+                    <ul class="name">
+                        <li>Recipe1</li>
+                        <li>Recipe2</li>
+                        <li>Recipe3</li>
+                        <li>Recipe4</li>
+                        <li>Recipe5</li>
+                    </ul>
+                    <ul class="date">
+                        <li>Approved</li>
+                        <li>Approved</li>
+						<li>Approved</li>
+						<li>Approved</li>
+						<li>Approved</li>
+                    </ul>
+                </div>
+            </div>
+    </main>
     </body>
     </html>
 
