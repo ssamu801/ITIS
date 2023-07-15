f881ffa87ad455a4aa6cddfe9c10a247cb5f2e73
ArnoldQuiros <arnoldlquiros@gmail.com>
2023-07-15 14:44:29 +0800

    Pretter Detailed Dishes Page generate reports now show "STOCK REPORT" even before any input

--- Owner/dishdetailed.php
+++ Owner/dishdetailed.php
@@ -17,16 +17,20 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name']))
     <?php @include '../navbar.php' ?>
     <div class="reportview">
         <h2> Dish Preview </h2>
+        <div class="detailedspecs">
         <!-- dish image -->
-        <img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="food img" style="width:100%; border-radius:10px">
-        <!-- dish ingridients  -->
-        <div>
-            <h3>Ingridients</h3>
-            <ul>
-                <li>Sago</li>
-                <li>Tinapay</li>
-            </ul>
+        <img src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        alt="food img" >
         </div>
+        <!-- dish ingridients  -->
+            <div class="detailedtext">
+                <h3>Ingredients</h3>
+                <ul>
+                    <li>Sago</li>
+                    <li>Tinapay</li>
+                </ul>
+            </div>    
+        
     </div>
 
     </body>
